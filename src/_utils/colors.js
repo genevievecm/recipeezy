26d9d70be5ad3d2995b27de0817ded6140db1bde
Genevieve Moreau <genevieve.c.moreau@gmail.com>
2020-10-26 10:25:01 -0400

    fix: re-add file for netlify

---
+++ src/_utils/colors.js
@@ -0,0 +1,21 @@
+export const blue = {
+    100: "#332265",
+    200: "#47D8E6"
+};
+
+export const neutral = {
+    100: "#ffffff",
+    200: "#f4f5f7",
+    300: "#e1e1e1",
+    400: "#737581",
+    500: "#4a4b53",
+    600: "#000000"
+};
+
+export const palette = {
+    primaryColor: blue[100],
+    secondaryColor: blue[200],
+    primaryHoverColor: blue[200],
+    primaryActiveColor: blue[100],
+    textColor: neutral[600],
+};
